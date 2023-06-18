6a4dd3782a6f72ae373e1279df9701f06d0fbe90
prismschism <sahit.felix91@gmail.com>
2023-06-17 22:14:35 -0700

    Completed Register to database process

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ import os
 import datetime
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
 from imports import usd
 import re
+import sqlite3
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 #Configure app
 app = Flask(__name__)
-
-# Configure database
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 # Custom filter
 app.jinja_env.filters["usd"] = usd
@@ -61,25 +58,61 @@ def register():
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
         # check that user inputs name, and valid password for registration
-        if not name:
-            flash("Missing Username!")
-        if len(password) < 8:
-            flash(f"Password must be at least 8 characters long!\n")
+        with sqlite3.connect("database.db") as db:
+             cursor = db.cursor()
 
-        if re.search('[0-9]', password) is None:
-            flash("Password must contain at least one number!")
-        if re.search('[A-Z]', password) is None:
-            flash("Password must contain at least one uppercase letter!")
-        if not password or password != confirmation:  # if password is empty or doesn't match confirmation return error/apology
-            flash("Password empty or does not match!")
+        try:
+            cursor.execute("SELECT * FROM users WHERE username = ?", (name,))
+            result = cursor.fetchall()
+            for i in result:
+                print(i[1])
+                if i[1] == name:
+                    flash("Username already taken! Choose a different Username!")
+                    print("equal")
+                    raise ValueError
+                else:
+                    
+                    print("no match")
+
+            if not name:
+                flash("Missing Username!")
+                raise ValueError
+            if len(password) < 8:
+                flash(f"Password must be at least 8 characters long!\n")
+                raise ValueError
+            if re.search('[0-9]', password) is None:
+                flash("Password must contain at least one number!")
+                raise ValueError
+            if re.search('[A-Z]', password) is None:
+                flash("Password must contain at least one uppercase letter!")
+                raise ValueError
+            if not password or password != confirmation:  # if password is empty or doesn't match confirmation return error/apology
+                flash("Password empty or does not match!")
+                raise ValueError
+            else:
+                pass
+
+        except ValueError:
+            print("Input/SelectValueError")
+            return render_template("register.html")
+
+        
 
         passhash = generate_password_hash(password)  # hash user's password
         try:
-            new_user = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", name, passhash)
+            cursor.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (name, passhash))
+            db.commit()
+            print(name, passhash)
+            print("Register/Insert success")
+
 
         except:
             flash("DatabaseError!")
+            print("Register/Insert Error")
             return render_template("register.html")
-        session["user_id"] = new_user
+
         flash("Registration Successful! WELCOME!")
+        
         return redirect("/")
+
+
